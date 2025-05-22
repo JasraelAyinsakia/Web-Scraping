1e65696dc19f6f8f48e08f3ab2b1bbb3c5e16da1
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-05-22 06:41:34 +0000

    SQL data types

--- example.py
+++ example.py
@@ -1,8 +1,15 @@
 import sqlite3
 
+# Establish a connection and a cursor
 connection = sqlite3.connect("data.db")
 cursor = connection.cursor()
 
-cursor.execute("SELECT * FROM events WHERE band='Lion'")
+# Query all data
+cursor.execute("SELECT * FROM events WHERE data='2024.09.03'")
 rows =cursor.fetchall()
 print(rows)
+# Query certian columns
+cursor.execute("SELECT band, date FROM events WHERE data='2024.07.02'")
+rows =cursor.fetchall()
+print(rows)
+
